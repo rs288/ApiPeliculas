9ef5c36cc20e8c3dd847122fe53e5faa74f05c69
rs288 <davids2804@pm.me>
2025-03-09 06:04:53 -0600

    video 34: Controlador, Metodos y vista Usuarios

---
+++ PeliculasWeb/Controllers/UsuariosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeliculasWeb.Models;
+using PeliculasWeb.Repositorio.IRepositorio;
+using PeliculasWeb.Utilidades;
+
+namespace PeliculasWeb.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly IUsuarioRepositorio _repoUsuario;
+
+        public UsuariosController(IUsuarioRepositorio repoUsuario)
+        {
+            _repoUsuario = repoUsuario;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Usuario() { });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTodosUsuarios()
+        {
+            return Json(new { data = await _repoUsuario.GetTodoAsync(CT.RutaUsuariosApi) });
+        }
+    }
+}
---
+++ PeliculasWeb/wwwroot/js/usuario.js
@@ -0,0 +1,20 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $("#tblUsuarios").DataTable({
+        "ajax": {
+            "url": "/Usuarios/GetTodosUsuarios",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "id", "width": "50%" },
+            { "data": "nombre", "width": "25%" },
+            { "data": "userName", "width": "25%" }
+        ]
+    });
+}
