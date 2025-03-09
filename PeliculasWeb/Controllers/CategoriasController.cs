28d8f4471bd0288a81bc6de5d90849d686716702
rs288 <davids2804@pm.me>
2025-03-09 00:02:01 -0600

    video 21: creacion del controlador categoriaControler y vista index

---
+++ PeliculasWeb/Controllers/CategoriasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeliculasWeb.Models;
+using PeliculasWeb.Repositorio.IRepositorio;
+
+namespace PeliculasWeb.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly ICategoriaRepositorio _repoCategoria;
+
+        public CategoriasController(ICategoriaRepositorio repoCategoria)
+        {
+            _repoCategoria = repoCategoria;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Categoria() { });
+        }
+    }
+}
