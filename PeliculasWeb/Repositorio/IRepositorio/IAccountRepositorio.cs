4b875cef3e05823c815692e1849f197c50183af7
rs288 <davids2804@pm.me>
2025-03-09 06:40:34 -0600

    video 36: intefaz para autentificacion

---
+++ PeliculasWeb/Repositorio/IRepositorio/IAccountRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using PeliculasWeb.Models;
+
+namespace PeliculasWeb.Repositorio.IRepositorio
+{
+    public interface IAccountRepositorio
+    {
+        Task<UsuarioAuth> LoginAsync(string url, UsuarioAuth itemCrear);
+        Task<bool> RegisterAsync(string url, UsuarioAuth itemCrear);
+    }
+}
