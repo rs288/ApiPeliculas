8098d86c7a495b42ef06a48b6a00ce1cda01f756
rs288 <davids2804@pm.me>
2025-03-09 06:36:21 -0600

    video 35: Modelo para autenticacion

---
+++ PeliculasWeb/Models/UsuarioAuth.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasWeb.Models
+{
+    public class UsuarioAuth
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "El usuario es obligatorio")]
+        public string NombreUsuario { get; set; }
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El password es obligatorio")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "El password debe estar entre 4 y 20 caracteres")]
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+}
